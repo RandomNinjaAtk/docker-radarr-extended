d6625417204988a8b2398f9f0ef3026ef73d837c
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-09-02 12:14:27 -0400

    v1.0.002 - Add log function, and log cleaner

--- root/scripts/QueueCleaner.bash
+++ root/scripts/QueueCleaner.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-scriptVersion="1.0.001"
+scriptVersion="1.0.002"
 
 if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
   arrUrlBase="$(cat /config/config.xml | xq | jq -r .Config.UrlBase)"
@@ -13,13 +13,25 @@ if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
   arrUrl="http://127.0.0.1:${arrPort}${arrUrlBase}"
 fi
 
+# auto-clean up log file to reduce space usage
+if [ -f "/config/logs/QueueCleaner.txt" ]; then
+	find /config/logs -type f -name "QueueCleaner.txt" -size +1024k -delete
+fi
+
+exec &>> "/config/logs/QueueCleaner.txt"
+chmod 666 "/config/logs/QueueCleaner.txt"
+
+log () {
+  m_time=`date "+%F %T"`
+  echo $m_time" :: "$1
+}
 
 arrQueueData="$(curl -s "$arrUrl/api/v3/queue?page=1&pagesize=1000000000&sortDirection=descending&sortKey=progress&includeUnknownMovieItems=true&apikey=${arrApiKey}" | jq -r .records[])"
 arrQueueIds=$(echo "$arrQueueData" | jq -r 'select(.status=="completed") | select(.trackedDownloadStatus=="warning") | .id')
 for queueId in $(echo $arrQueueIds); do
   arrQueueItemData="$(echo "$arrQueueData" | jq -r "select(.id==$queueId)")"
   arrQueueItemTitle="$(echo "$arrQueueItemData" | jq -r .title)"
-  echo "Removing Failed Queue ID: $queueId ($arrQueueItemTitle) from Radarr Queue..."
+  log "Removing Failed Queue ID: $queueId ($arrQueueItemTitle) from Radarr Queue..."
   curl -sX DELETE "$arrUrl/api/v3/queue/$queueId?removeFromClient=true&blocklist=true&apikey=${arrApiKey}"
 done
 
