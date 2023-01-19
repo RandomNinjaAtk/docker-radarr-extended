ef916bf8415c28b67bd5cf7106a30543f22067c8
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-19 07:40:45 -0500

    Change for deprecated option

--- root/scripts/Recyclarr.bash
+++ root/scripts/Recyclarr.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-scriptVersion="1.0.003"
+scriptVersion="1.0.004"
 
 if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
   arrUrlBase="$(cat /config/config.xml | xq | jq -r .Config.UrlBase)"
@@ -40,7 +40,7 @@ fi
 
 # update radarr
 log "Updating Radarr via Recyclarr"
-/recyclarr/recyclarr radarr -c /config/extended/configs/recyclarr.yaml --app-data /recyclarr-data
+/recyclarr/recyclarr sync radarr -c /config/extended/configs/recyclarr.yaml --app-data /recyclarr-data
 log "Complete"
 
 exit
